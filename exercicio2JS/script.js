c7d5c8bdcc49ae8d0099a4e0a0cf525e1fab0932
Raul Falcao de Araujo <ra278711@students.ic.unicamp.br>
2026-05-22 10:39:23 -0300

    Terminado a lista de tarefas

--- exercicio2JS/script.js
+++ exercicio2JS/script.js
@@ -10,13 +10,18 @@ function criarCaixinha(textoDaTarefa){ // Crio uma caixinha que será o comparti
     list_item.botao.textContent = "OK"
     list_item.botao.className = "bg-green-400 px-2 rounded border-2 border-green-500 text-white hover:text-green-800 hover:border-green-800 rounded-md"
     list_item.botao.addEventListener("click", () => {
-        list_item.texto.classList.toggle("line-through")
-    }) 
+        list_item.texto.classList.toggle("line-through") //toggle recebe um atributo por vez!
+        list_item.texto.classList.toggle("font-bold")
+        list_item.botao.classList.toggle("bg-red-400")
+        list_item.botao.classList.toggle("border-black") 
     // Anteriormente, o problema esta a referência. O botão não sabia qual 'caixinha' alterar. Para isso, 
     // usamos o o método '.classList', que retorna os atributos de classe de texto. Além disso,
     // o '.toggle("line-through")', ele varia a presença de um elemento. Ou seja, se ele não existe, ele 
     // cria, e se ele existe, ele remove. Nesse caso, o atributo de classe a ser "variado" é o "line-through",que traça 
     // uma linha através dos elementos, indicando que ele a tarefa foi completa. 
+    
+    }) 
+    
     list_item.texto.textContent = textoDaTarefa // Texto da tarefa é a tarefa em si. Ex.: jogar bola,comer pizza, etc...
     list_item.li.appendChild(list_item.texto) //Nessa linha, tornamos o 'atributo texto' como filho da div de contação 
     list_item.li.appendChild(list_item.botao) //Nessa linha, tornamos o 'atributo botao' como filho da div de contação 
@@ -33,11 +38,20 @@ function adicionar(){
     } else{
     tarefas.push(tarefaAdicionada) 
     // 2. Aviso ao usuário que deu certo e "zero" o texto da caixinha 
-    window.alert("Tarefa registrada") 
     tarefa.value = " "
     // 3. Crio um elemento na página que registra /o que foi digitado
     //item = document.createElement("li")/
     let caixinha = criarCaixinha(tarefaAdicionada) 
     lista.appendChild(caixinha.li) 
+    }
+}
+function finalizar(){
+    // Quando o botão "FINALIZAR" é clicado, a nossa lista de afazeres diários será zerada
+    if(tarefas.length!=0){
+        tarefas = [] // Reinicializo a minha lista de tarefas 'por baixo dos panos' (ainda não está visível na tela)
+        window.alert("Você cumpriu todas as tarefas do seu dia!Vamos para mais?")
+        lista.remove() // 'remove()' apaga todos os elementos filhos de um elemento pai
+    } else{
+        window.alert("Adicione tarefas a sua lista para finalizar")
     }
 }
