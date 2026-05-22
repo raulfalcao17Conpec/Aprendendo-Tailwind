73026567e6a9d565579eede9bf9ec0b827303fce
Raul Falcao de Araujo <ra278711@students.ic.unicamp.br>
2026-05-22 10:05:52 -0300

    Aprendendo novos métodos

--- exercicio2JS/script.js
+++ exercicio2JS/script.js
@@ -7,18 +7,22 @@ function criarCaixinha(textoDaTarefa){ // Crio uma caixinha que será o comparti
         texto: document.createElement("span"),
         botao: document.createElement("button")
     }
-
-    list_item.texto.textContent = textoDaTarefa // Texto da tarefa é a tarefa em si. Ex.: jogar bola,comer pizza, etc...
     list_item.botao.textContent = "OK"
-    list_item.botao.className = "bg-green-400 px-2 rounded bg-blue-300 text-white rounded-md"
-    // list_item.botao.addEventListener("click", marcarFeita) // Adicionamos o 'click' ao botão criado
+    list_item.botao.className = "bg-green-400 px-2 rounded border-2 border-green-500 text-white hover:text-green-800 hover:border-green-800 rounded-md"
+    list_item.botao.addEventListener("click", () => {
+        list_item.texto.classList.toggle("line-through")
+    }) 
+    // Anteriormente, o problema esta a referência. O botão não sabia qual 'caixinha' alterar. Para isso, 
+    // usamos o o método '.classList', que retorna os atributos de classe de texto. Além disso,
+    // o '.toggle("line-through")', ele varia a presença de um elemento. Ou seja, se ele não existe, ele 
+    // cria, e se ele existe, ele remove. Nesse caso, o atributo de classe a ser "variado" é o "line-through",que traça 
+    // uma linha através dos elementos, indicando que ele a tarefa foi completa. 
+    list_item.texto.textContent = textoDaTarefa // Texto da tarefa é a tarefa em si. Ex.: jogar bola,comer pizza, etc...
     list_item.li.appendChild(list_item.texto) //Nessa linha, tornamos o 'atributo texto' como filho da div de contação 
     list_item.li.appendChild(list_item.botao) //Nessa linha, tornamos o 'atributo botao' como filho da div de contação 
     list_item.li.className="w-full flex flex row justify-between"
-    
     return list_item
 }
-
 
 function adicionar(){
     let tarefa = document.querySelector("input#tarefa") // O quereySelector recebe um parametro do tipo "tag#id"
